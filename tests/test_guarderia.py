ba73fff08c83534a8b85c1f2c9ba172dca2811f1
Edison236 <edisonavendano1@gmail.com>
2024-11-12 23:19:29 -0500

    Se aguarda el archivo de gitIgnore y se ajusta guarderia

--- tests/test_guarderia.py
+++ tests/test_guarderia.py
@@ -1,19 +1,23 @@
 import unittest
 from models.guarderia import Guarderia
+from models.huron import Huron
 
 class TestGuarderia(unittest.TestCase):
 
     def setUp(self):
-        self.boas = Guarderia()
+        self.guarderia = Guarderia()
     
     def tearDown(self):
         pass
 
     def test_feed_boa(self):
         for i in range(12):
-            response = self.boas.feed_boa(1)
+            response = self.guarderia.feed_boa(1)
             print(response)
-        response_two = self.boas.feed_boa(5)
+        response_two = self.guarderia.feed_boa(5)
         print(response_two)
-        response_three = self.boas.feed_boa(None)
+        response_three = self.guarderia.feed_boa(None)
         print(response_three)
+
+    def test_show_huron(self):
+        print(self.guarderia.huron[0].__dict__)
